4a57aa4fc48a7b8e863cbd24a7eb17efb1ea1474
EyuReaper <eyureaper@gmail.com>
2025-10-01 09:01:00 +0300

    🎨 Fix(format code): move link to proper section & add attribute
    - the stylesheet link is properly nested & added precedence=default to handle stylesheet loading order.

--- yckf-frontend/src/app/layout.tsx
+++ yckf-frontend/src/app/layout.tsx
@@ -14,7 +14,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="h-full">
-      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" />
+      <head>
+        <link 
+          rel="stylesheet" 
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" 
+          precedence="default"
+        />
+      </head>
       <body className="flex flex-col min-h-screen antialiased">
         <ThemeProvider>{children}</ThemeProvider>
       </body>
