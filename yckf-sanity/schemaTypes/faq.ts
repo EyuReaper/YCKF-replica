726bc0e390fd123e68873b2e1132bf482792d8df
EyuReaper <eyureaper@gmail.com>
2025-10-15 07:38:04 +0300

    ✨ Feat(schema): add FAQ schema

---
+++ yckf-sanity/schemaTypes/faq.ts
@@ -0,0 +1,33 @@
+import { defineField, defineType } from 'sanity';
+
+export default defineType({
+  name: 'faq',
+  title: 'FAQ',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+      validation: (Rule) => Rule.required(),
+    }),
+    defineField({
+      name: 'answer',
+      title: 'Answer',
+      type: 'text',
+      validation: (Rule) => Rule.required(),
+    }),
+    defineField({
+      name: 'category',
+      title: 'Category',
+      type: 'string',
+      options: {
+        list: [
+          { title: 'Beginner', value: 'beginner' },
+          { title: 'Advanced', value: 'advanced' },
+        ],
+      },
+      validation: (Rule) => Rule.required(),
+    }),
+  ],
+});
